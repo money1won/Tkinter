762a686d07d7b596f83a80358f6900a472d52930
money1won <44730985+money1won@users.noreply.github.com>
2020-03-22 20:32:41 -0500

    Add files via upload

---
+++ Console.py
@@ -0,0 +1,30 @@
+import tkinter
+from itertools import cycle
+
+
+class App(tkinter.Tk):
+    def __init__(self, textList, master=None):
+        tkinter.Tk.__init__(self, master)
+        self.textiter = cycle(textList)
+        self.txt = tkinter.StringVar()
+        self.rootEntry = tkinter.Entry(self, textvariable=self.txt)
+        self.rootEntry.pack(side="bottom")
+        self.rootEntry.bind("<Return>", self.cycle_text)
+        self.rootText = tkinter.Text(self)
+        self.rootText.pack()
+        self.rootText.bind("<Insert>", self.insert_all)
+        self.newList = []
+
+    def cycle_text(self, arg=None):
+        t = self.textiter.next()
+        self.txt.set(t)
+        self.rootText.insert("end", t + "\n")
+        self.newList.append(self.rootText.get("end - 2 chars linestart", "end - 1 chars"))
+
+    def insert_all(self, arg):
+        self.rootText.insert("end", "".join([s.strip() for s in self.newList]))
+
+
+textList = ["", "Line 2", "Line 3"]
+root = App(textList)
+root.mainloop()
